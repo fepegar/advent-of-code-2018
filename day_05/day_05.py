07313f541f6b0e7eb1426240952f43575b391658
Fernando Perez-Garcia <fepegar@gmail.com>
2018-12-05 09:16:01 +0000

    Update skeleton

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -1,4 +1,10 @@
 
+from pprint import pprint
+
+
+def p(*args, verbose=True):
+    if verbose:
+        pprint(*args)
 
 
 def read_input(filepath):
@@ -18,17 +24,20 @@ def part_2(data):
 
 
 if __name__ == "__main__":
-    print('Part 1')
-    example_1 = part_1('example_1.txt')
-    print('Example:', example_1)
-    answer_1 = part_1('input.txt')
-    print('Answer:', answer_1)
+    verbose = True
 
-    print()
+    data = read_input('input.txt')
 
-    print('Part 2')
-    example_2 = part_2('example_2.txt')
-    print('Example:', example_2)
-    answer_2 = part_2('input.txt')
-    print('Answer:', answer_2)
+    p('Part 1', verbose=verbose)
+    example_1 = part_1(read_input('example_1.txt'))
+    p('Example:', example_1, verbose=verbose)
+    answer_1 = part_1(data)
+    p('Answer:', answer_1, verbose=verbose)
 
+    p(verbose=verbose)
+
+    p('Part 2', verbose=verbose)
+    example_2 = part_2(read_input('example_2.txt'))
+    p('Example:', example_2, verbose=verbose)
+    answer_2 = part_2(data)
+    p('Answer:', answer_2, verbose=verbose)
