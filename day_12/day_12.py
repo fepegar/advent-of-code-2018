33f135f6d9b6315a01dda5e28bd3954513b29243
Fernando Perez-Garcia <fepegar@gmail.com>
2018-12-09 11:40:22 +0000

    Change repo skeleton

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -25,19 +25,20 @@ def part_2(data):
 if __name__ == "__main__":
     verbose = True
 
+    example = read_input('example.txt')
     data = read_input('input.txt')
 
     p('Part 1', verbose=verbose)
-    example_1 = part_1(read_input('example_1.txt'))
-    p('Example:', example_1, verbose=verbose)
+    example_1 = part_1(example)
+    p('Example 1:', example_1, verbose=verbose)
     answer_1 = part_1(data)
-    p('Answer:', answer_1, verbose=verbose)
+    p('Answer 1:', answer_1, verbose=verbose)
 
     p(verbose=verbose)
 
     p('Part 2', verbose=verbose)
-    example_2 = part_2(read_input('example_2.txt'))
-    p('Example:', example_2, verbose=verbose)
+    example_2 = part_2(example)
+    p('Example 2:', example_2, verbose=verbose)
     answer_2 = part_2(data)
-    p('Answer:', answer_2, verbose=verbose)
+    p('Answer 2:', answer_2, verbose=verbose)
 
--- make_skeleton.py
+++ make_skeleton.py
@@ -27,29 +27,31 @@ def part_2(data):
 if __name__ == "__main__":
     verbose = True
 
+    example = read_input('example.txt')
     data = read_input('input.txt')
 
     p('Part 1', verbose=verbose)
-    example_1 = part_1(read_input('example_1.txt'))
-    p('Example:', example_1, verbose=verbose)
+    example_1 = part_1(example)
+    p('Example 1:', example_1, verbose=verbose)
     answer_1 = part_1(data)
-    p('Answer:', answer_1, verbose=verbose)
+    p('Answer 1:', answer_1, verbose=verbose)
 
     p(verbose=verbose)
 
     p('Part 2', verbose=verbose)
-    example_2 = part_2(read_input('example_2.txt'))
-    p('Example:', example_2, verbose=verbose)
+    example_2 = part_2(example)
+    p('Example 2:', example_2, verbose=verbose)
     answer_2 = part_2(data)
-    p('Answer:', answer_2, verbose=verbose)
+    p('Answer 2:', answer_2, verbose=verbose)
 
 """
 
-for i in range(25):
+for i in range(8, 25):
     day_string = f'day_{i + 1:02d}'
     day_dir = Path(day_string)
+    day_dir.mkdir(exist_ok=True)
     code_path = day_dir / f'{day_string}.py'
     code_path.write_text(TEMPLATE)
-    files = 'input', 'example_1', 'example_2'
+    files = 'input', 'example'
     for filename in files:
         (day_dir / f'{filename}.txt').touch()
