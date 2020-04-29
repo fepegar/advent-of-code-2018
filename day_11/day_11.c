982f6f6bc1d365d5a82508f014676a7cfc982054
Fernando Perez-Garcia <fepegar@gmail.com>
2020-04-29 22:58:47 +0100

    Add stuff that might not work

--- day_11/day_11.c
+++ day_11/day_11.c
@@ -3,67 +3,133 @@
 
 #define MATRIX_SIZE 300
 
-void *find_largest_power(int serial_number, int *largest_coordinates)
+int find_largest_power(int matrix[][MATRIX_SIZE], int *largest_coordinates, int size)
 {
-    int matrix[MATRIX_SIZE][MATRIX_SIZE];
     int largest_power = 0;
-    int rack_id;
     int power;
-    int aux;
-    int debug = 0;
-    int xd = 33;
-    int yd = 45;
-    for (int y = 1; y <= MATRIX_SIZE; y++)
+    int x, y;
+
+    fill_matrix(matrix, 3214);
+
+    for (int i = 0; i < MATRIX_SIZE - 2; i++)
     {
-        for (int x = 1; x <= MATRIX_SIZE; x++)
+        for (int j = 0; j < MATRIX_SIZE - 2; j++)
         {
-            rack_id = x + 10;
-            if (x == xd && y == yd && debug) printf("Rack ID: %d\n", rack_id);
-            power = rack_id * y;
-            if (x == xd && y == yd && debug) printf("Power: %d\n", power);
-            power += serial_number;
-            if (x == xd && y == yd && debug) printf("Power: %d\n", power);
-            power *= rack_id;
-            if (x == xd && y == yd && debug) printf("Power: %d\n", power);
-            power = (power / 100) % 10;  // 12345 -> 123 -> 3
-            if (x == xd && y == yd && debug) printf("Power: %d\n", power);
-            power -= 5;
-            if (x == xd && y == yd && debug) printf("Power: %d\n", power);
+            power = 0;
+            for (int di = 0; di < size; di++)
+            {
+                for (int dj = 0; dj < size; dj++)
+                {
+                    power += matrix[i + di][j + dj];
+                    if (size == 4 && power > 1000)
+                    {
+                        printf("WAAT\n");
+                        printf("Power: %d\n", power);
+                        printf("Value: %d\n", matrix[i + di][j + dj]);
+                        printf("i,j: %d,%d\n\n", i, j);
+                    }
+                }
+            }
+            // printf("%d ", power);
             if (power > largest_power)
             {
-                printf("Largest: %d\n", largest_power);
-                largest_power = power;
+                x = j + 1;
+                y = i + 1;
                 largest_coordinates[0] = x;
                 largest_coordinates[1] = y;
+                largest_power = power;
             }
-            // printf("Power: %d\n", power);
+        }
+    }
+    // printf("largest_power: %d\n", largest_power);
+    return largest_power;
+}
+
+
+void fill_matrix(int matrix[][MATRIX_SIZE], int serial_number)
+{
+    int rack_id;
+    int power;
+    int x, y;
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            x = j + 1;
+            y = i + 1;
+            rack_id = x + 10;
+            power = rack_id * y;
+            power += serial_number;
+            power *= rack_id;
+            power = (power / 100) % 10;  // 12345 -> 123 -> 3
+            power -= 5;
+            matrix[i][j] = power;
+            if (power > 1000) {
+                printf("OMG");
+                printf("i, j: %d,%d\n", i, j);
+                printf("power: %d\n\n", power);
+            }
         }
     }
 }
 
 
-void *part_1(int serial_number, int *answer)
+void find_largest_size(int matrix[][MATRIX_SIZE], int *answer)
 {
-    find_largest_power(serial_number, answer);
+    int power;
+    int largest_power = 0;
+    int largest_power_size = 0;
+    int coordinates[2];
+    for (int size = 1; size <= MATRIX_SIZE; size++)
+    {
+        power = find_largest_power(matrix, coordinates, size);
+        printf("Power: %d\n", power);
+        if (power > largest_power)
+        {
+            largest_power = power;
+            answer[0] = coordinates[0];
+            answer[1] = coordinates[1];
+            answer[2] = size;
+            printf("X,Y,size: %d,%d,%d\n\n", answer[0], answer[1], answer[2]);
+        }
+    }
 }
 
 
-int part_2(int serial_number)
+void part_1(int matrix[][MATRIX_SIZE], int *answer)
 {
-    int answer = -1;
-    return answer;
+    int any_size = 0;
+    find_largest_power(matrix, answer, 3);
+}
+
+
+void part_2(int matrix[][MATRIX_SIZE], int *answer)
+{
+    int any_size = 1;
+    find_largest_size(matrix, answer);
 }
 
 
 int main(int argc, char *argv[])
 {
-    int data;
-    if (argc < 2)
-        data = 3214;
-    else
-        data = atoi(argv[1]);
-    printf("Part 1:\n");
+    int serial_number;
+    int matrix[MATRIX_SIZE][MATRIX_SIZE];
     int answer_1[2];
-    part_1(data, answer_1);
+    int answer_2[3];
+
+    if (argc < 2)
+        serial_number = 3214;
+    else
+        serial_number = atoi(argv[1]);
+
+    fill_matrix(matrix, serial_number);
+
+    printf("Part 1:\n");
+    part_1(matrix, answer_1);
     printf("Answer 1: %d,%d\n", answer_1[0], answer_1[1]);
+
+    printf("Part 2:\n");
+    part_2(matrix, answer_2);
+    printf("Answer 2: %d,%d,%d\n", answer_2[0], answer_2[1], answer_2[2]);
+    return 0;
 }
--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,5 +1,7 @@
-
+from re import findall
 from pathlib import Path
+from copy import deepcopy
+from collections import deque
 
 this_dir = Path(__file__).parent
 verbose = True
@@ -12,11 +14,111 @@ def p(*args):
 
 def read_input(relative_filepath):
     filepath = this_dir / relative_filepath
-    return filepath.read_text()
+    text = filepath.read_text()
+    state = findall('initial state: ([#.]+)', text)[0]
+    plants = [char == '#' for char in state]
+    pots = [Pot(plant, i) for (i, plant) in enumerate(plants)]
+    rules_pattern = r'([#.]+) .+ ([#.]).*'
+    rules = [Rule(*groups) for groups in findall(rules_pattern, text)]
+    return pots, rules
+
+
+class Rule:
+    def __init__(self, rule_string, result_char):
+        self.string = rule_string
+        self.result = result_char
+
+    def __repr__(self):
+        return f'{self.string}: {self.result}'
+
+
+class Pot:
+    def __init__(self, plant, number):
+        self.plant = plant
+        self.number = number
+
+    def __repr__(self):
+        return '#' if self.plant else '.'
+
+    def get_neighborhood(self, pots):
+        for i, pot in enumerate(pots):
+            if pot.number == self.number:
+                index_pots = i
+                break
+        neighbors = pots[index_pots - 2:index_pots + 3]
+        neighborhood = ['#' if pot.plant else '.' for pot in neighbors]
+        return ''.join(neighborhood)
+
+
+    def evolve(self, pots, rule):
+        neighborhood = self.get_neighborhood(pots)
+        grow = rule.string == neighborhood
+        return grow
+
+
+def get_idx_extreme_pot(pots, left):
+    for i, pot in enumerate(pots):
+        if pot.plant:
+            numbers = i, pot.number
+            if left:
+                return numbers
+    return numbers
+
+
+def expand_pots(pots):
+    pots = deque(pots)
+    expand_pots_left(pots)
+    expand_pots_right(pots)
+    return list(pots)
+
+
+def expand_pots_left(pots):
+    index, pot_number = get_idx_extreme_pot(pots, left=True)
+    needed = 2 - index
+    for i in range(needed):
+        new_pot_number = pot_number - (i + 1)
+        pot = Pot(plant=False, number=new_pot_number)
+        pots.appendleft(pot)
+
+
+def expand_pots_right(pots):
+    index, pot_number = get_idx_extreme_pot(pots, left=False)
+    needed = index + 2 - len(pots) + 1
+    for i in range(needed):
+        new_pot_number = pot_number + (i + 1)
+        pot = Pot(plant=False, number=new_pot_number)
+        pots.append(pot)
+
+
+def print_pots(pots):
+    for pot in pots:
+        char = '#' if pot.plant else '.'
+        char = pot.number
+        print(char, end=' ')
+    print()
+
+
+def evolve(pots, rules, epochs=1):
+    for _ in range(epochs):
+        pots = expand_pots(pots)
+        print_pots(pots)
+        new_pots = deepcopy(pots)
+        for new_pot in new_pots[2:-2]:
+            for rule in rules:
+                grow = new_pot.evolve(pots, rule)
+                if grow:
+                    new_pot.plant = True
+                    break
+            else:
+                new_pot.plant = False
+        pots = new_pots
+    return pots
 
 
 def part_1(data):
-    answer = None
+    pots, rules = data
+    pots = evolve(pots, rules, epochs=20)
+    answer = sum(pot.number for pot in pots if pot.plant)
     return answer
 
 
@@ -27,19 +129,18 @@ def part_2(data):
 
 if __name__ == "__main__":
     example = read_input('example.txt')
-    data = read_input('input.txt')
+#     data = read_input('input.txt')
 
     p('Part 1')
     example_1 = part_1(example)
     p('Example 1:', example_1)
-    answer_1 = part_1(data)
-    p('Answer 1:', answer_1)
+#     answer_1 = part_1(data)
+#     p('Answer 1:', answer_1)
 
-    p()
+#     p()
 
-    p('Part 2')
-    example_2 = part_2(example)
-    p('Example 2:', example_2)
-    answer_2 = part_2(data)
-    p('Answer 2:', answer_2)
-
+#     p('Part 2')
+#     example_2 = part_2(example)
+#     p('Example 2:', example_2)
+#     answer_2 = part_2(data)
+#     p('Answer 2:', answer_2)
